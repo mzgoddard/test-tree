22b5242ed363d652edf30bfb1a7e370606ca7778
Michael "Z" Goddard <mzgoddard@gmail.com>
2022-01-13 08:55:58 -0500

    unify-editor iteration

--- unify-editor.ts
+++ unify-editor.ts
@@ -1,1 +1,18 @@
-import {} from "./unify-view";
+import * as React from "react";
+
+import { Arg, Facts } from "./unify-view";
+
+const MAX_ARGS = 32;
+function* args() {
+  for (let i = 0; i < MAX_ARGS; i++) {
+    yield new Arg();
+  }
+  throw new Error("too many args");
+}
+
+const [component] = args();
+
+const BASIC_COMPONENTS = new Facts()
+  .add(["component", "text", {}, component])
+  .add(["component", "button", {}, component])
+  .add(["component", "field", {}, component]);
